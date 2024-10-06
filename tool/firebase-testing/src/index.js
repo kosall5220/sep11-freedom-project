2f79ed66181ce2a8a54c12efcd4b710335d388c6
Kosal Lour <kosal.lour@gmail.com>
2024-10-06 17:04:16 -0400

    setup firebase

--- tool/firebase-testing/src/index.js
+++ tool/firebase-testing/src/index.js
@@ -1,1 +1,40 @@
-console.log("Hello from index.js");
+
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyBYQHPMys5ykuDu7Z94hPAAwoueF3BSQcE",
+    authDomain: "fir-9-test-1.firebaseapp.com",
+    projectId: "fir-9-test-1",
+    storageBucket: "fir-9-test-1.appspot.com",
+    messagingSenderId: "665870590130",
+    appId: "1:665870590130:web:816761dc5ab86e0e2529ab"
+  };
+
+  // init firebase app
+  initiallizeApp(firebaseConfig)
+
+  // init services
+  const db = getFirestore()
+
+  const db = firebase.firestore()
+
+  db.collection("books")
+
+  // collection ref
+  const colRef = collection(db, 'books')
+
+  // get collection data
+  getDocs(colRef)
+  .then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+        books.push({ ...doc.data(), id: doc.id})
+        console.log(books)
+    })
+  })
+
+  .catch(err => {
+    console.log(err.message)
+  })
